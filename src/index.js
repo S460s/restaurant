58a8d3224f34404a9fdb85fcb04c8a78b31149bf
S460 <aleksandar.ta6ev@gmail.com>
2020-12-04 19:15:53 +0200

    Made a factory function for the tabs

--- src/index.js
+++ src/index.js
@@ -1,43 +1,34 @@
 import { homepage } from "./homepage";
 
+const elementFactory = function (name, elmnt, id, cls, txt) {
+	const displayElement = function () {
+		name = document.createElement(`${elmnt}`);
+		name.textContent = `${txt}`;
+		name.setAttribute("id", `${id}`);
+		name.classList.add(`${cls}`);
+		navBar.appendChild(name);
+	};
+
+	return { displayElement };
+};
+
 const domManipulation = (function () {
 	const content = document.getElementById("content");
 	const navBar = document.createElement("div");
+	const homeTab = elementFactory("homeTab", "p", "homeTab", "tab", "Home");
+	const menuTab = elementFactory("menuTab", "p", "menuTab", "tab", "Menu");
+	const contactTab = elementFactory("contactTab", "p", "contactTab", "tab", "Contacts");
 
 	const createNavBar = function () {
 		navBar.setAttribute("id", "navBar");
 		content.appendChild(navBar);
 	};
 
-	const createHomeTab = function () {
-		const homeTab = document.createElement("p");
-		homeTab.textContent = "Home";
-		homeTab.setAttribute("id", "homeTab");
-		homeTab.classList.add("tab");
-		navBar.appendChild(homeTab);
-	};
-
-	const createMenuTab = function () {
-		const menuTab = document.createElement("p");
-		menuTab.textContent = "Menu";
-		menuTab.setAttribute("id", "menuTab");
-		menuTab.classList.add("tab");
-		navBar.appendChild(menuTab);
-	};
-
-	const createContactTab = function () {
-		const contactTab = document.createElement("p");
-		contactTab.textContent = "Contacts";
-		contactTab.setAttribute("id", "homeTab");
-		contactTab.classList.add("tab");
-		navBar.appendChild(contactTab);
-	};
-
 	const generatePage = function () {
 		createNavBar();
-		createHomeTab();
-		createMenuTab();
-		createContactTab();
+		homeTab.displayElement();
+		menuTab.displayElement();
+		contactTab.displayElement();
 	};
 
 	return { generatePage };
